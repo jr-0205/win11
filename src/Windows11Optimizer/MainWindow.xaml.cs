fd097379bf9c5847022a8ec4e2597351fe2b827f
Carlos <carlossama359@gmail.com>
2026-09-20 08:45:45 -0600

    Fix SelectionChanged namespace for AI recommendation UI

--- src/Windows11Optimizer/MainWindow.xaml.cs
+++ src/Windows11Optimizer/MainWindow.xaml.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using System.Security.Principal;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
